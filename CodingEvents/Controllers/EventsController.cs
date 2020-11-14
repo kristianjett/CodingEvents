c9f11392a87600dd190c512860d5b8daa6a28662
Kristian <jettkristian@gmail.com>
2020-11-14 13:07:26 -0800

    Added Edit functionality

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -10,13 +10,13 @@ namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        static private List<Event> Events = new List<Event>(); 
-
         // Get: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
             ViewBag.events = EventData.GetAll();
+
+            //List<Event> events = new List<Event>(EventData.GetAll());
 
             return View();
         }
@@ -28,13 +28,13 @@ namespace CodingEvents.Controllers
         }
 
         [HttpPost("/Events/Add")]
-        public IActionResult NewEvent(Event newEvent)
+        public IActionResult NewEvent(string name, string description)
         {
+            Event newEvent = new Event(name, description);
             EventData.Add(newEvent);
 
             return Redirect("/Events");
         }
-
         public IActionResult Delete()
         {
             ViewBag.events = EventData.GetAll();
@@ -48,6 +48,23 @@ namespace CodingEvents.Controllers
             {
                 EventData.Remove(eventId);
             }
+
+            return Redirect("/Events");
+        }
+
+        [Route("/events/edit/{eventId}")]
+        public IActionResult Edit(int eventId)
+        {
+            ViewBag.eventToEdit = EventData.GetById(eventId);
+            return View();
+        }
+
+        [HttpPost("/Events/Edit")]
+        public IActionResult SubmitEditEventForm(int eventId, string name, string description)
+        {
+            //TODO add controller code
+            EventData.GetById(eventId).Name = name;
+            EventData.GetById(eventId).Description = description;
 
             return Redirect("/Events");
         }
