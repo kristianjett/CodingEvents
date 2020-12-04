23204fd50245acf89285c846e9e3f5feef8a80b3
Kristian <jettkristian@gmail.com>
2020-12-04 00:27:16 -0800

    Completed exercise to add EventCategory class & View

---
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
+using CodingEvents.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<EventCategory> categories = context.EventCategories.ToList();
+
+            return View(categories);
+        }
+    }
+}
