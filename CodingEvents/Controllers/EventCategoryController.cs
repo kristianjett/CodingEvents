f9ea74d9c750fd1d5013e50fc33a7b05eb898ec7
Kristian <jettkristian@gmail.com>
2020-12-04 01:06:59 -0800

    Completed studio

--- CodingEvents/Controllers/EventCategoryController.cs
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -25,5 +25,53 @@ namespace CodingEvents.Controllers
 
             return View(categories);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+
+            return View(addEventCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+                };
+
+                context.EventCategories.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("/EventCategory");
+            }
+
+            return View(addEventCategoryViewModel);
+        }
+
+        public IActionResult Delete()
+        {
+            List<EventCategory> deletableCategories = context.EventCategories.ToList();
+            return View(deletableCategories);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds)
+            {
+                //EventData.Remove(eventId);
+                EventCategory categoryToDelete = context.EventCategories.Find(eventId);
+                context.EventCategories.Remove(categoryToDelete);
+            }
+
+            context.SaveChanges();
+
+            return Redirect("/EventCategory");
+        }
     }
 }
---
+++ CodingEvents/ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.ViewModels
+{
+    public class AddEventCategoryViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 20 characters")]
+        public string Name { get; set; }
+    }
+}
